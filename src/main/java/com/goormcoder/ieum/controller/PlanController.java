605aa0bd5b82c61d9f297ba2664d06c75fb23be6
ISMINIMIN <summmm.224@gmail.com>
2024-07-17 21:30:46 +0900

    feat: 일정 멤버 추가 구현

--- src/main/java/com/goormcoder/ieum/controller/PlanController.java
+++ src/main/java/com/goormcoder/ieum/controller/PlanController.java
@@ -2,6 +2,7 @@ package com.goormcoder.ieum.controller;
 
 
 import com.goormcoder.ieum.dto.request.PlanCreateDto;
+import com.goormcoder.ieum.dto.request.PlanMemberCreateDto;
 import com.goormcoder.ieum.service.PlanService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -9,10 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -30,6 +28,13 @@ public class PlanController {
         UUID memberId = UUID.fromString(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         planService.createPlan(planCreateDto, memberId);
         return ResponseEntity.status(HttpStatus.OK).body("일정이 생성되었습니다.");
+    }
+
+    @PostMapping("/{planId}/invite")
+    @Operation(summary = "멤버 추가", description = "일정 멤버를 추가합니다.")
+    public ResponseEntity<String> createPlan(@PathVariable Long planId, @RequestBody PlanMemberCreateDto planMemberCreateDto) {
+        planService.createPlanMember(planId, planMemberCreateDto.memberLoginIds());
+        return ResponseEntity.status(HttpStatus.OK).body("멤버가 추가되었습니다.");
     }
 
 }
--- src/main/java/com/goormcoder/ieum/service/PlanService.java
+++ src/main/java/com/goormcoder/ieum/service/PlanService.java
@@ -5,11 +5,14 @@ import com.goormcoder.ieum.domain.Plan;
 import com.goormcoder.ieum.domain.PlanMember;
 import com.goormcoder.ieum.dto.request.PlanCreateDto;
 import com.goormcoder.ieum.repository.PlanRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +27,28 @@ public class PlanService {
         Plan plan = Plan.of(dto.location(), dto.startedAt(), dto.endedAt(), dto.vehicle());
         plan.addPlanMember(PlanMember.of(plan, member));
         planRepository.save(plan);
+    }
+
+    @Transactional
+    public void createPlanMember(Long planId, String[] memberLoginIds) {
+        Plan plan = findByPlanId(planId);
+
+        List<Member> members = Stream.of(memberLoginIds)
+                .map(memberService::findByLoginId)
+                .toList();
+
+        for(Member member : members) {
+            plan.addPlanMember(PlanMember.of(plan, member));
+        }
+
+        System.out.println(plan.getPlanMembers());
+
+        planRepository.save(plan);
+    }
+
+    private Plan findByPlanId(Long planId) {
+        return planRepository.findById(planId)
+                .orElseThrow(() -> new EntityNotFoundException("해당 일정은 존재하지 않습니다."));
     }
 
 }
