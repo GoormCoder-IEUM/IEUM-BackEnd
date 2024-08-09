f71055ecb2a264c6d67199d58aed454000dc2b29
ISMINIMIN <summmm.224@gmail.com>
2024-08-09 18:08:16 +0900

    refactor: 코드 중복 제거 및 성능 개선

--- src/main/java/com/goormcoder/ieum/repository/CategoryRepository.java
+++ src/main/java/com/goormcoder/ieum/repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.goormcoder.ieum.repository;
 
 import com.goormcoder.ieum.domain.Category;
+import com.goormcoder.ieum.domain.enumeration.CategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Boolean existsByCategoryType(CategoryType categoryType);
+
 }
--- src/main/java/com/goormcoder/ieum/service/DataInitializerService.java
+++ src/main/java/com/goormcoder/ieum/service/DataInitializerService.java
@@ -2,17 +2,14 @@ package com.goormcoder.ieum.service;
 
 import com.goormcoder.ieum.config.DestinationDescriptions;
 import com.goormcoder.ieum.domain.Category;
-import com.goormcoder.ieum.domain.enumeration.CategoryType;
 import com.goormcoder.ieum.domain.Destination;
+import com.goormcoder.ieum.domain.enumeration.CategoryType;
 import com.goormcoder.ieum.domain.enumeration.DestinationName;
 import com.goormcoder.ieum.repository.CategoryRepository;
 import com.goormcoder.ieum.repository.DestinationRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +19,25 @@ public class DataInitializerService {
     private final DestinationRepository destinationRepository;
     private final DestinationDescriptions descriptions;
 
+    private final CategoryType[] categoryTypes = {
+            CategoryType.ATTRACTION,
+            CategoryType.FOOD,
+            CategoryType.ACCOMMODATION
+    };
+
+    private final DestinationName[] destinationNames = {
+            DestinationName.JEJU,
+            DestinationName.BUSAN,
+            DestinationName.SEOUL,
+            DestinationName.GYEONGJU,
+            DestinationName.GANGNEUNG,
+            DestinationName.YEOSU,
+            DestinationName.JEONJU,
+            DestinationName.POHANG,
+            DestinationName.INCHEON,
+            DestinationName.DAEJEON
+    };
+
     @PostConstruct
     public void init() {
         createCategories();
@@ -29,33 +45,19 @@ public class DataInitializerService {
     }
 
     public void createCategories() {
-        if(categoryRepository.count() == 0) {
-            List<Category> categories = Arrays.asList(
-                    Category.of(CategoryType.ATTRACTION),
-                    Category.of(CategoryType.FOOD),
-                    Category.of(CategoryType.ACCOMMODATION)
-            );
-            
-            categoryRepository.saveAll(categories);
+        for(CategoryType categoryType : categoryTypes) {
+            if(!categoryRepository.existsByCategoryType(categoryType)) {
+                categoryRepository.save(Category.of(categoryType));
+            }
         }
     }
 
     public void createDestinations() {
-        if(destinationRepository.count() == 0) {
-            List<Destination> destinations = Arrays.asList(
-                    Destination.of(DestinationName.JEJU, descriptions.getDescription().get(DestinationName.JEJU.toString())),
-                    Destination.of(DestinationName.BUSAN, descriptions.getDescription().get(DestinationName.BUSAN.toString())),
-                    Destination.of(DestinationName.SEOUL, descriptions.getDescription().get(DestinationName.SEOUL.toString())),
-                    Destination.of(DestinationName.GYEONGJU, descriptions.getDescription().get(DestinationName.GYEONGJU.toString())),
-                    Destination.of(DestinationName.GANGNEUNG, descriptions.getDescription().get(DestinationName.GANGNEUNG.toString())),
-                    Destination.of(DestinationName.YEOSU, descriptions.getDescription().get(DestinationName.YEOSU.toString())),
-                    Destination.of(DestinationName.JEONJU, descriptions.getDescription().get(DestinationName.JEONJU.toString())),
-                    Destination.of(DestinationName.POHANG, descriptions.getDescription().get(DestinationName.POHANG.toString())),
-                    Destination.of(DestinationName.INCHEON, descriptions.getDescription().get(DestinationName.INCHEON.toString())),
-                    Destination.of(DestinationName.DAEJEON, descriptions.getDescription().get(DestinationName.DAEJEON.toString()))
-            );
-
-            destinationRepository.saveAll(destinations);
+        for(DestinationName destinationName : destinationNames) {
+            if(!destinationRepository.existsByDestinationName(destinationName)) {
+                String description = descriptions.getDescription().get(destinationName.toString());
+                destinationRepository.save(Destination.of(destinationName, description));
+            }
         }
     }
 
